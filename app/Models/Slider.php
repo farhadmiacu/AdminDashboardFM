f3a57b75cb2087a8012d013a346e917245e7a79c
farhadmiacu <farhadmia.cu@gmail.com>
2026-03-13 02:21:04 +0600

    Slider Module Added

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Slider extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'page_position',
+        'image',
+        'status',
+    ];
 }
--- database/migrations/2026_03_12_001510_create_sliders_table.php
+++ database/migrations/2026_03_12_001510_create_sliders_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('sliders', function (Blueprint $table) {
             $table->id();
+            $table->string('page_position')->nullable(); // home, about, contact, etc.
+            $table->string('image')->nullable();
+            $table->tinyInteger('status')->default(1);
             $table->timestamps();
         });
     }
